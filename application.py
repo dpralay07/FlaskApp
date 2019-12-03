51caa902a2044ed69040f8e6f02cc44a69add235
Pralay Kumar Das <pralaykumar.das@citrix.com>
2019-12-03 16:12:31 +0530

    Updated the endpoint respons

--- application.py
+++ application.py
@@ -1,15 +1,15 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello! Welcome to Azure."
+    return "Hello! Welcome to Azure Application of GS Bot."
 
 @app.route("/home")
 def home():
-    return "Hello! Welcome to the Home page of GSBot."
+    return jsonify({'status': 'success', 'description': 'Endpoint is working fine.'})
 
 
 if __name__ == "__main__":
